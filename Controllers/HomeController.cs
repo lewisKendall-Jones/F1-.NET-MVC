b6eb19ac94578d7fd51a2fd91609f9724cab8acd
lewisKendall-Jones <60380722+lewisKendall-Jones@users.noreply.github.com>
2022-12-09 17:09:52 +1300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Team()
+        {
+            ViewBag.Message = "The current team:";
+
+            return View();
+        }
+
+        public ActionResult Copyright()
+        {
+            return View();
+        }
+
+        public ActionResult Store()
+        {
+            return View();
+        }
+
+        public ActionResult Results()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assignment3.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }    
+
+        public virtual ICollection<Result> Results { get; set; }
+    }
+}
